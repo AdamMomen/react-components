53cc0469fe6015e4e265fcb811ed6d5df7401a17
Adam <adammuman81@gmail.com>
2020-12-12 10:18:07 +0100

    add types to plopfile.ts

--- plopfile.ts
+++ plopfile.ts
@@ -2,7 +2,7 @@ import { NodePlopAPI } from 'plop';
 import { createComponentGenerator } from './templates/generators/component';
 import { upperCamelCase } from './templates/helpers';
 
-export default function (plop) {
+export default function (plop: NodePlopAPI) {
 
   plop.setHelper('upperCamelCase', upperCamelCase);
 
